8bbdb2e6e595db4b1c9982afbd1b511b66b0eef9
Christian Henrik Reich <Havehave@gmail.com>
2015-04-09 20:25:42 +0200

    Refactoring succeded

--- blobstorage/blobstorage.go
+++ blobstorage/blobstorage.go
@@ -11,3 +11,13 @@ type BlobStorage struct {
 func New(http *gohaveazurestoragecommon.HTTP) *BlobStorage {
 	return &BlobStorage{http: http}
 }
+
+func (blobStorage *BlobStorage) CreateContainer(containerName string) (httpStatusCode int) {
+	_, httpStatusCode = blobStorage.http.Request("PUT", containerName, "?restype=container", nil, map[string]string{}, false, false, false)
+	return httpStatusCode
+}
+
+func (blobStorage *BlobStorage) DeleteContainer(containerName string) (httpStatusCode int) {
+	_, httpStatusCode = blobStorage.http.Request("DELETE", containerName, "?restype=container", nil, map[string]string{}, false, false, false)
+	return httpStatusCode
+}
