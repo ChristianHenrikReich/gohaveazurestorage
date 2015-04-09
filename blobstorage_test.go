ec56902919d0b3214142ce05d2f6e4bfa9d41d2e
Christian Henrik Reich <Havehave@gmail.com>
2015-04-09 19:09:50 +0200

    Adding test for refactoring

---
+++ blobstorage_test.go
@@ -0,0 +1,16 @@
+package gohaveazurestorage
+
+import "testing"
+
+func TestRefactor(t *testing.T) {
+	container := "ContainerForTestingACLMethods"
+
+	goHaveAzureStorage := NewWithDebug(Account, Key, true)
+	blobStorage := goHaveAzureStorage.BlobStorage()
+
+	httpStatusCode := blobStorage.CreateContainer(container)
+	assertHTTPStatusCode(t, httpStatusCode, 201)
+
+	httpStatusCode = blobStorage.DeleteContainer(container)
+	assertHTTPStatusCode(t, httpStatusCode, 202)
+}
